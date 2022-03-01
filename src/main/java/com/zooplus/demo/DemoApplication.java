7db211747ca6021373c9588c354cdbfb66ff0270
anton-gorokh <rerearitet@gmail.com>
2022-03-01 18:01:32 +0100

    Spring initializr init commit

---
+++ src/main/java/com/zooplus/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.zooplus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
